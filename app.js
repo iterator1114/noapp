508ef2d387d1fc8cb69df57863e7edcb39bd0a00
Abhishek Kumar <87389313+iterator1114@users.noreply.github.com>
2022-12-17 11:19:48 +0530

    update

--- app.js
+++ app.js
@@ -17,9 +17,10 @@ app.use(cookieParser());
 app.set('view engine', 'ejs');
 
 // database connection
+const port = process.env.PORT || 3000;
 const dbURI = 'mongodb+srv://Abhishek:abhishek@cluster0.r09h5ok.mongodb.net/db?retryWrites=true&w=majority';
 mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex:true })
-  .then((result) => app.listen(3000))
+  .then((result) => app.listen(port))
   .catch((err) => console.log(err));
 
 // routes
